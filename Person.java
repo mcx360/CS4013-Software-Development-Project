1696081dbe33f57ffb1ff96ba597fcf0a64eb0b9
MLmarbles <118482430+MLmarbles@users.noreply.github.com>
2023-11-20 20:34:06 +0000

    Create Person.java

---
+++ Person.java
@@ -0,0 +1,9 @@
+public abstract class Person {
+    private String name;
+    private String email;
+
+    public Person (String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+}
