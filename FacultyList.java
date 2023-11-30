a83a06fa5f872e88b2f1eb1736765d7510537bb3
MLmarbles <118482430+MLmarbles@users.noreply.github.com>
2023-11-30 23:29:24 +0000

    Create FacultyList.java

---
+++ FacultyList.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class FacultyList {
+    private static FacultyList instance = null;
+    private List<Faculty> faculties;
+    protected ProgrammeList programmeList;
+
+    public FacultyList() {
+        faculties = new ArrayList<>();
+        Faculty arts = new Faculty("Faculty of Arts, Humanities, and Social Sciences");
+        Faculty educ = new Faculty("Faculty of Education and Health Science");
+        Faculty busi = new Faculty("Kemmy Business School");
+        Faculty scie = new Faculty("Faculty of Science And Engineering");
+        Faculty worl = new Faculty("Irish World Academy of Music & Dance");
+        
+        faculties.add(arts);
+        faculties.add(educ);
+        faculties.add(busi);
+        faculties.add(scie);
+        faculties.add(worl);
+
+        programmeList = new ProgrammeList();
+    }
+
+    public static FacultyList getInstance() {
+        if (instance == null) {
+            instance = new FacultyList();
+        }
+        return instance;
+    }
+    
+    public List<Faculty> getFaculties() {
+        return faculties;
+    }
+
+    public Faculty findFacultyByName(String facultyName) {
+        for (Faculty faculty : faculties) {
+            if (faculty.getFacultyName().equals(facultyName)) {
+                return faculty;
+            }
+        }
+        return null;
+    }
+}
