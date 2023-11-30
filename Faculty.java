bb8ce06cfac19fbaf1c34f80e8aee7e67693fdda
MLmarbles <118482430+MLmarbles@users.noreply.github.com>
2023-11-30 23:29:04 +0000

    Create Faculty.java

---
+++ Faculty.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+
+public class Faculty {
+    private String facultyName;
+    private ArrayList<String> facultyEmailList;
+    private ArrayList<Department> departments;
+    protected FacultyList facultyList; // Reference to FacultyList
+
+    public Faculty(String facultyName) {
+        this.facultyName = facultyName;
+        this.facultyEmailList = new ArrayList<>();
+        this.departments = new ArrayList<>();
+        this.facultyList = FacultyList.getInstance();
+    }
+
+    public void addDepartments(Department department) {
+        departments.add(department);
+    }
+
+    public void deleteDepartment(Department department) {
+        departments.remove(department);
+    }
+
+    public void addFacultyEmail(String facultyEmail){
+        facultyEmailList.add(facultyEmail);
+    }
+
+    public void deleteFacultyEmail(String facultyEmail){
+        facultyEmailList.remove(facultyEmail);
+    }
+
+    public Department findDepartmentByName(String DepartmentName) {
+        for (Department department : departments) {
+            if (department.getDepartmentName().equals(DepartmentName)) {
+                return department;
+            }
+        }
+        return null;
+    }
+
+    public String getFacultyName() {
+        return this.facultyName;
+    }
+}
