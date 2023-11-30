5837fc600dc8c7288247161a2b1c97227780cd1a
MLmarbles <118482430+MLmarbles@users.noreply.github.com>
2023-11-30 23:28:44 +0000

    Update Department.java

--- Department.java
+++ Department.java
@@ -1,31 +1,87 @@
 import java.util.ArrayList;
-import java.util.List;
+public class Department{
+    private String departmentName;
+    private ArrayList<Programme> departmentProgrammes;
+    private ArrayList<String> departmentFaculty;
+    private Faculty faculty;
 
-public class Department {
-    private List<Programme> programmes;
-
-    public Department() {
-        programmes = new ArrayList<>();
+    public Department(String departmentName, Faculty faculty){
+        this.departmentName = departmentName;
+        this.faculty=faculty;
+        departmentProgrammes = new ArrayList<>();
+        departmentFaculty = new ArrayList<>();
+    }
+    
+    public Department(String departmentName,ArrayList<Programme> departmentProgrammes,Faculty faculty){
+        this.faculty = faculty;
+        this.departmentName = departmentName;
+        this.departmentProgrammes = departmentProgrammes;
+        departmentFaculty = new ArrayList<>();
     }
 
-    public void addProgramme(Programme programme) {
-        programmes.add(programme);
+    public Department(String departmentName,ArrayList<Programme> departmentProgrammes,ArrayList<String> departmentFaculty,Faculty faculty){
+        this.faculty = faculty;
+        this.departmentName = departmentName;
+        this.departmentProgrammes = departmentProgrammes;
+        this.departmentFaculty = departmentFaculty;
     }
 
-    public void removeProgramme(Programme programme) {
-        programmes.remove(programme);
+    public String getDepartmentName(){
+        return this.departmentName;
+    }
+
+    public Faculty getFaculty() {
+        return this.faculty;
+    }
+
+    public void addProgramme(Programme programme){
+        departmentProgrammes.add(programme);
+        this.faculty.facultyList.programmeList.addProgramme(programme);
+    }
+
+    public void cancelProgramme(Programme programme){
+        departmentProgrammes.remove(programme);
+    }
+
+    public ArrayList<Programme> getDepartmentProgrammes(){
+        return departmentProgrammes;
+    }
+
+    public void addFaculty(String name){
+        departmentFaculty.add(name);
+    }
+
+    public void removeFaculty(String name){
+        departmentFaculty.remove(name);
+    }
+
+    @Override
+    public String toString(){
+        return "Department: "+departmentName +"\n"+departmentProgrammesToString()+"\n Deparment Programmes: \n"+departmentProgrammesToString()+"\n"+departmentFacultyToString(); 
+    }
+
+    public String departmentProgrammesToString(){
+        String Programmes = "";
+        for(int i=0;i<departmentProgrammes.size();i++){
+            Programmes+=i+1+"."+departmentProgrammes.get(i)+"\n";
+        }
+        return Programmes;
+    }
+
+    public String departmentFacultyToString(){
+        String departmentFacultyMembers = "Department faculty: \n";
+        for(int i=0;i<departmentFaculty.size();i++){
+            departmentFacultyMembers+=departmentFaculty.get(i)+"\n";
+        }
+        return departmentFacultyMembers+"\n";
     }
 
     public Programme findProgrammeByName(String programmeName) {
-        for (Programme programme : programmes) {
+        for (Programme programme: departmentProgrammes) {
             if (programme.getProgrammeName().equals(programmeName)) {
                 return programme;
             }
         }
         return null;
-    }
-
-    public List<Programme> getAllProgrammes() {
-        return programmes;
     }
 }
