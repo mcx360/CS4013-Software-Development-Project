ca01b77f356136963e712647c5de6a1651cff360
MLmarbles <118482430+MLmarbles@users.noreply.github.com>
2023-12-01 15:53:49 +0000

    Update Module.java

--- Module.java
+++ Module.java
@@ -4,26 +4,49 @@ import java.util.Map;
 import java.util.TreeMap;
 
 public class Module{
+    private int year,semester;
     private String moduleName;
     private String moduleCode;
     private String professor;
     private int credits;
     private ArrayList<Student> studentsInModule;
     private TreeMap<Student, String> studentGrades = new TreeMap<>();
-    //private TreeMap<Student, Double> studentResultInPercentage;
     private GradingSystem moduleGradingSystem;
-    private boolean moduleRepeat = false;
+    private boolean pass;
+    private boolean compensatingFail;
 
     
     //This constructor will make the module have the default grading system for the university
-    public Module(String moduleName,String moduleCode,int credits,Programme programme,String professor){
+    public Module(int moduleMapCode,String moduleName,String moduleCode,int credits,Programme programme,String professor){
         this.moduleName =moduleName;
         this.moduleCode = moduleCode;
         this.credits = credits;
         this.studentsInModule = programme.getStudents(); //By default each student in the programme is added to the module, incase a student has to be removed or a student from another programme needs to be added there are mutator methods provided
         this.moduleGradingSystem = new GradingSystem();
         this.professor = professor;
+        this.year=splitInteger(moduleMapCode)[0];
+        this.semester=splitInteger(moduleMapCode)[1];
         
+    }
+
+    public Module(String moduleName,String moduleCode,int credits,Programme programme,String professor, int year, int semester){
+        this.moduleCode = moduleCode;
+        this.credits = credits;
+        this.studentsInModule = programme.getStudents(); //By default each student in the programme is added to the module, incase a student has to be removed or a student from another programme needs to be added there are mutator methods provided
+        this.moduleGradingSystem = new GradingSystem();
+        this.professor = professor;
+        this.year = year;
+        this.semester = semester;
+    }
+
+    public static int[] splitInteger(int number) {
+        number = number % 100;
+
+        int tens = number / 10;
+        int units = number % 10;
+
+        int[] result = { tens, units };
+        return result;
     }
 
     //In the event we want to add someone outside the programme to the module, we do it using this method
@@ -42,7 +65,11 @@ public class Module{
                 studentsInModule.remove(i);
                 return;
             }
+        }
     }
+
+    public ArrayList<Student> getStudentsInModule(){
+        return studentsInModule;
     }
 
     public String getStudentGrade(Student student){
@@ -81,29 +108,67 @@ public class Module{
 
     //we give grade as a percentage, the program converts it into a grade
     public void setStudentGrade(Student student, Double grade){
-        studentGrades.put(student,moduleGradingSystem.getGrade(grade));
-        //studentResultInPercentage.put(student,grade);
+        studentGrades.put(student,GradingSystem.getGrade(grade));
+        if(getStudentGrade(student)=="NG"){
+            this.pass=false;
+            this.compensatingFail=false;
+        }
+        else if(getStudentGrade(student)=="F"){
+            this.pass=false;
+            this.compensatingFail=false;
+        }
+        else if(getStudentGrade(student)=="D1"){
+            this.pass = false;
+            this.compensatingFail = true;
+        }
+        else if(getStudentGrade(student)=="D2"){
+            this.pass = false;
+            this.compensatingFail = true;
+        }
+        else{
+            this.pass = true;
+            this.compensatingFail = false;
+        }
     }
 
-    /*public Double getMouduleAvg(){
+    //returns average for moudule in the qca format e.g average=2.8
+    public Double getMouduleAvg(){
         ArrayList<Double> studentResults = new ArrayList<Double>();
-        for(Map.Entry<Student, Double> entry : studentResultInPercentage.entrySet()){
-            studentResults.add(entry.getValue()); 
+        for(Map.Entry<Student, String> entry : studentGrades.entrySet()){
+            studentResults.add(getStudentGradeDouble(entry.getValue())); 
         }
 
         Double total = 0.0;
         for(int i=0;i<studentResults.size();i++){
-            total=+studentResults.get(i);
+            total+=studentResults.get(i);
         }
         return total/studentResults.size();
-        
     }
-    */
-    /* public Double getModuleAvg(){
-        ArrayList<Double> studentResults = new ArrayList<Double>();
-    } */ 
 
     public int getCredits () {
         return this.credits;
     }
+
+    @Override
+    public String toString(){
+        return moduleName+"\n"+moduleCode+"\n"+"Credits: "+credits+"\n"+"Professor: "+professor+"\n";
+    }
+
+    public String getModuleName(){
+        return moduleName;
+    }
+
+    public String getModuleCode(){
+        return moduleCode;
+    }
+
+    public int getYear() {
+    	return this.year;
+    }
+    
+    public int getSemester() {
+    	return this.semester;
+    }
+    
+    
 }
